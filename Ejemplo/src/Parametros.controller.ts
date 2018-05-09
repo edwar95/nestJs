587b7c093d213569b58c279863180be53f2f03d7
david <davidmoralesp1995@gmail.com>
2018-05-09 11:57:42 -0500

    @Header

--- Ejemplo/src/Parametros.controller.ts
+++ Ejemplo/src/Parametros.controller.ts
@@ -1,10 +1,10 @@
-import {Body, Controller, Get, Param, Post, Query, Req, Res} from "@nestjs/common";
+import {Body, Controller, Get, Param, Post, Query, Req, Res, Headers} from "@nestjs/common";
 
 
 @Controller('Parametros')
 export class ParametrosController{
     @Post('devolver/:id')
-    devolverParametros(@Req() request, @Res() response, @Query()queryParams, @Body()bodyParams, @Param paramsParam ){
+    devolverParametros(@Req() request, @Res() response, @Query()queryParams, @Body()bodyParams, @Param() paramsParam ){
         const respuesta={
             queryParams:queryParams,
             bodyParams:bodyParams,
@@ -15,7 +15,7 @@ export class ParametrosController{
 
     }
     @Get('ReqRes')
-    requestResponse(@Req() request, @Res() response){
+    requestResponse(@Req() request, @Res() response, @Headers() headers){
         const respuesta={
             baseUrl: request.baseUrl,
             hostname:request.hostname,
@@ -25,7 +25,7 @@ export class ParametrosController{
             originalUrl:request.originalUrl,
             path:request.path,
             protocol:request.protocol,
-
+            headers:request.header
         };
         return response.sen(respuesta);
 
