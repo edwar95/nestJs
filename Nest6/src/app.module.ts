a6a6336f7bcf5e032c056f19438be54ff7fe9682
david <davidmoralesp1995@gmail.com>
2018-05-16 13:01:53 -0500

    cookies

--- Nest6/src/app.module.ts
+++ Nest6/src/app.module.ts
@@ -1,14 +1,37 @@
-import {Module} from '@nestjs/common';
+import {Module, NestModule, MiddlewareConsumer} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {UsuarioController} from "./usuario.controller";
 import {UsuarioService} from "./usuario.service";
 import {ParametrosController} from "./parametros.controller";
+import {LogMiddleware} from './log.middleware';
 
 @Module({
     imports: [],
-    controllers: [AppController,UsuarioController, ParametrosController],
-    providers: [AppService, UsuarioService],
+    controllers: [
+        AppController,
+        UsuarioController,
+        ParametrosController],
+    providers: [
+        AppService,
+        UsuarioService
+    ],
 })
-export class AppModule {
+export class AppModule implements NestModule {
+    nombreAplicacion = 'EPN';
+
+    configure(consumer: MiddlewareConsumer)
+        : void {
+        consumer
+            .apply(LogMiddleware)
+            .with(this.nombreAplicacion, 1989)
+            .forRoutes(
+                UsuarioController,
+                AppController,
+                ParametrosController
+            )
+        //.apply(OtroMiddleware)
+        //.forRoutes(Otras rutas);
+    }
+
 }
