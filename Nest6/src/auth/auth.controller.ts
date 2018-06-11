f11b73921d0c0857c3bed80a3b5cb8979ef5257b
david <davidmoralesp1995@gmail.com>
2018-06-11 10:04:37 -0500

    jwt service

---
+++ Nest6/src/auth/auth.controller.ts
@@ -0,0 +1,33 @@
+import {BadRequestException, Body, Controller, Post} from "@nestjs/common";
+import {JwtService} from "../Servicios/jwt.service";
+
+@Controller('Auth')
+export class AuthController{
+    constructor(private _jwtService: JwtService){
+    }
+
+    @Post('login')
+    login(@Body('username') username:string , @Body('password') password:string   ){
+        const enviaUsername= username;
+        const enviaPassword= password;
+        const enviarParametros= enviaPassword && enviaUsername;
+
+        if(enviarParametros){
+            if(username==='adrianeguez'&& password==='1234'){
+                const payload={
+                    username: username
+                };
+
+                return this._jwtService.emitirToken(payload);
+                
+            }else{
+                throw new BadRequestException({mensaje:'Credenciales Erroneas'})
+            }
+        }else{
+            throw new BadRequestException({mensaje:'No envia parametros'});
+
+        }
+    }
+
+
+}
